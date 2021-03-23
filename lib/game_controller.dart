70b1b627d4d420416c55a9e72d666924717b832b
Gustavo Pavez Gikis <soulandbone@gmail.com>
2021-03-23 20:06:30 +0200

    First part of game app with Spawn method.

---
+++ lib/game_controller.dart
@@ -0,0 +1,87 @@
+import 'dart:math';
+import 'dart:ui';
+
+import 'package:flame/flame.dart';
+import 'package:flame/game.dart';
+import 'package:flutter/gestures.dart';
+import 'package:game_app/components/health_bar.dart';
+
+import './components/player.dart';
+import './components/enemy.dart';
+
+class GameController extends Game {
+  Random rand;
+  Size screenSize;
+  double tileSize;
+  Player player;
+  List<Enemy> enemies;
+  HealthBar healthBar;
+
+  GameController() {
+    initialize();
+  }
+
+  void initialize() async {
+    resize(await Flame.util.initialDimensions());
+    player = Player(this);
+    enemies = [];
+    healthBar = HealthBar(this);
+  }
+
+  void render(Canvas c) {
+    Rect background = Rect.fromLTWH(0, 0, screenSize.width, screenSize.height);
+    Paint backgroundPaint = Paint()..color = Color(0xFFFAFAFA);
+    c.drawRect(background, backgroundPaint);
+
+    player.render(c);
+    enemies.forEach((Enemy enemy) => enemy.render(c));
+    healthBar.render(c);
+  }
+
+  void update(double t) {
+    enemies.forEach((Enemy enemy) => enemy.update(t));
+    player.update(t);
+    healthBar.update(t);
+  }
+
+  void resize(Size size) {
+    screenSize = size;
+    tileSize = screenSize.width / 10;
+  }
+
+  void onTapDown(TapDownDetails d) {
+    print(d.globalPosition);
+    enemies.forEach((Enemy enemy) {
+      if (enemy.enemyRect.contains(d.globalPosition)) {
+        enemy.onTapDown();
+      }
+    });
+  }
+
+  void spawnEnemy() {
+    double x, y;
+    switch (rand.nextInt(4)) {
+      case 0:
+        //Top
+        x = rand.nextDouble() * screenSize.width;
+        y = -tileSize * 2.5;
+        break;
+      case 1:
+        //Right
+        x = screenSize.width + tileSize * 2.5;
+        y = rand.nextDouble() * screenSize.height;
+        break;
+      case 2:
+        //Bottom
+        x = rand.nextDouble() * screenSize.width;
+        y = screenSize.height + tileSize * 2.5;
+        break;
+      case 3:
+      //Left 
+      x = -tileSize * 2.5;
+      y = rand.nextDouble() * screenSize.height;
+
+      case 4:
+    }
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flame/util.dart';
+import 'package:flutter/gestures.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter/widgets.dart';
+
+import './game_controller.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  Util flameUtil = Util();
+  await flameUtil.fullScreen();
+  await flameUtil.setOrientation(DeviceOrientation.portraitUp);
+
+  GameController gameController = GameController();
+
+  runApp(gameController.widget);
+
+  TapGestureRecognizer tapper = TapGestureRecognizer();
+  tapper.onTapDown = gameController.onTapDown;
+  // ignore: deprecated_member_use
+  flameUtil.addGestureRecognizer(tapper);
+}
